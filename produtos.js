c4847cedd0ef9452824d51d40f9caed39f8c0a46
artcagnacci <artcagnacci@hotmail.com>
2016-10-26 16:11:48 -0200

    Update and rename README.md to produtos.js

---
+++ produtos.js
@@ -0,0 +1,11 @@
+module.exports = function (app) {
+	app.get('/produtos', function (req,res) {
+		var connection = app.infra.connectionFactory();
+		var produtosBanco = new app.infra.ProdutosDAO(connection);
+		produtosBanco.lista(function(err,results){
+			console.log(err)
+		res.render('produtos/lista',{lista:results});
+		});
+		connection.end();
+	});
+};
